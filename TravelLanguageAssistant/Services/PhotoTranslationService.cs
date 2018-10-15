0c67bcf5aec46b55b863920d5c8d180b7d614b09
Luce Carter <luce@lucyscarter.co.uk>
2018-10-15 08:16:07 +0100

    OCR working

---
+++ TravelLanguageAssistant/Services/PhotoTranslationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+using System.IO;
+namespace TravelLanguageAssistant.Services
+{
+	public class PhotoTranslationService
+	{
+		public PhotoTranslationService()
+		{
+		}
+
+
+	}
+}
--- TravelLanguageAssistant/ViewModels/TranslateImageViewModel.cs
+++ TravelLanguageAssistant/ViewModels/TranslateImageViewModel.cs
@@ -5,6 +5,17 @@ using Xamarin.Forms;
 using Microsoft.AppCenter.Analytics;
 using Plugin.Media;
 using Xamarin.Essentials;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using TravelLanguageAssistant.Services;
+using System.IO;
+using System.Threading.Tasks;
+using Plugin.Media.Abstractions;
+using System.Dynamic;
+using System.Text;
+
 
 namespace TravelLanguageAssistant.ViewModels
 {
@@ -13,39 +24,69 @@ namespace TravelLanguageAssistant.ViewModels
 
 		public TranslateImageViewModel()
 		{
-
+			TakePhotoTranslateCommand = new Command(async () => await TakePhotoAsync());
 		}
 
 		public Image image;
 
-		public ICommand TakePhotoTranslateCommand
+		MediaFile photo;
+		StreamImageSource imageSource;
+
+		public StreamImageSource ImageForAnalysis
 		{
-			get => new Command(async () =>
-			{
-				CrossMedia.Current.Initialize();
-
-				if (CrossMedia.Current.IsCameraAvailable || CrossMedia.Current.IsTakePhotoSupported)
-				{
-					var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
-					{
-						Directory = "Travel Language Assistant",
-						Name = "ImageForTranslation.jpg"
-					});
-
-					if (file == null) return;
-
-				}
-				else
-				{
-					Application.Current.MainPage.DisplayAlert("No Camera", "No Camera Available.", "OK");
-					return;
-				}
-
-			});
+			get => imageSource;
+			set => SetProperty(ref imageSource, value);
 		}
 
+		public ICommand TakePhotoTranslateCommand { get; }
+		private async Task TakePhotoAsync()
+		{
+			try
+			{
+				if (CrossMedia.Current.IsCameraAvailable)
+				{
+					photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+					{
+						PhotoSize = PhotoSize.Small
+					});
+
+					ImageForAnalysis = (StreamImageSource)ImageSource.FromStream(() => photo.GetStream());
+
+					ComputerVisionClient computerVisionClient = new ComputerVisionClient(new ApiKeyServiceClientCredentials(AzureKeys.AzureComputerVisonAPIKey));
+					computerVisionClient.Endpoint = AzureKeys.AzureComputerVisionEndpoint;
+
+					var stuff = await computerVisionClient.RecognizePrintedTextInStreamAsync(true, photo.GetStream(), OcrLanguages.Unk);
+					var language = stuff.Language;
+
+					var sb = new StringBuilder();
+					foreach (var region in stuff.Regions)
+					{
+						foreach (var line in region.Lines)
+						{
+							foreach (var word in line.Words)
+							{
+								sb.Append(" " + word.Text);
+							}
+						}
+					}
+					AnalysedTextResult = sb.ToString();
+				}
+			}
+
+			catch (Exception e)
+			{
+				await Application.Current.MainPage.DisplayAlert("Error", e.Message, "OK");
+			}
+		}
+
+		string textResult;
+		public String AnalysedTextResult
+		{
+			get => textResult;
+			set => SetProperty(ref textResult, value);
+		}
+
+
+
 	}
-
-
 }
-
