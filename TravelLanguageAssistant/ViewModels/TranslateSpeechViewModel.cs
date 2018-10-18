ee0471fe7ddcd69d224a6bfcf9b4a22c405e3ecf
Luce Carter <luce@lucyscarter.co.uk>
2018-10-18 09:50:26 +0100

    Added speech recording ability in code behind

--- TravelLanguageAssistant/ViewModels/TranslateSpeechViewModel.cs
+++ TravelLanguageAssistant/ViewModels/TranslateSpeechViewModel.cs
@@ -1,21 +1,58 @@
 using System;
 using Xamarin.Forms;
 using Microsoft.AppCenter.Analytics;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using Plugin.AudioRecorder;
+using MvvmHelpers;
 
 namespace TravelLanguageAssistant.ViewModels
 {
-	public class TranslateSpeechViewModel
+	public class TranslateSpeechViewModel : BaseViewModel
 	{
-		string
-		public Command RecordConversationCommand
+		private string _translateButtonText = "Record Conversation to Translate";
+		public string RecordForTranslateButton
 		{
-			get
+			get => _translateButtonText;
+			set => SetProperty(ref _translateButtonText, value);
+		}
+
+		public TranslateSpeechViewModel()
+		{
+			RecordConversationCommand = new Command(async () => await RecordAudio());
+		}
+		public ICommand RecordConversationCommand { get; }
+
+		private async Task RecordAudio()
+		{
+			RecordForTranslateButton = "Recording in Progress";
+			var recorder = new AudioRecorderService
 			{
-				return new Command(() =>
+				StopRecordingOnSilence = true,
+				StopRecordingAfterTimeout = true,
+				TotalAudioTimeout = TimeSpan.FromSeconds(15)
+			};
+
+			try
+			{
+				if (!recorder.IsRecording)
 				{
-					Application.Current.MainPage.DisplayAlert("Record Clicked!", "", "OK");
-					Analytics.TrackEvent("Record Speech Clicked!");
-				});
+					var recordTask = await recorder.StartRecording();
+					var audioFile = await recordTask;
+					if (audioFile != null)
+					{
+						Application.Current.MainPage.DisplayAlert("Woo", audioFile, "OK");
+					}
+				}
+				else
+				{
+					await recorder.StopRecording();
+					RecordForTranslateButton = "Record Conversation to Translate";
+				}
+			}
+			catch (Exception ex)
+			{
+				await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
 			}
 		}
 	}
--- TravelLanguageAssistant/Views/TranslateSpeechPage.xaml.cs
+++ TravelLanguageAssistant/Views/TranslateSpeechPage.xaml.cs
@@ -3,15 +3,60 @@ using System.Collections.Generic;
 
 using Xamarin.Forms;
 using TravelLanguageAssistant.ViewModels;
+using Plugin.AudioRecorder;
+using System.Threading.Tasks;
 
 namespace TravelLanguageAssistant.Views
 {
 	public partial class TranslateSpeechPage : ContentPage
 	{
+		AudioRecorderService recorder;
+
 		public TranslateSpeechPage()
 		{
 			InitializeComponent();
-			BindingContext = new TranslateSpeechViewModel();
+
+			recorder = new AudioRecorderService
+			{
+				StopRecordingOnSilence = true,
+				StopRecordingAfterTimeout = true,
+				TotalAudioTimeout = TimeSpan.FromSeconds(15),
+				AudioSilenceTimeout = TimeSpan.FromSeconds(2)
+			};
+		}
+
+		async void Record_Clicked(object sender, EventArgs e)
+		{
+			await RecordAudio();
+		}
+
+		async Task RecordAudio()
+		{
+			try
+			{
+				if (!recorder.IsRecording)
+				{
+					RecordButton.IsEnabled = false;
+
+					var audioRecordTask = await recorder.StartRecording();
+					RecordButton.Text = "Stop Recording";
+					RecordButton.IsEnabled = true;
+
+					var recordResult = await audioRecordTask;
+
+					RecordButton.Text = "Record";
+				}
+				else
+				{
+					RecordButton.IsEnabled = false;
+					await recorder.StopRecording();
+					RecordButton.IsEnabled = true;
+				}
+			}
+			catch (Exception e)
+			{
+				Application.Current.MainPage.DisplayAlert("Error", e.Message, "OK");
+			}
 		}
 	}
 }
