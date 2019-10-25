fd776c9effcd8239dfe1cd426f2a8b9f6c09ae55
Luce Carter <luce@lucyscarter.co.uk>
2019-10-25 16:08:27 +0200

    Updated with Shell during Cologne Xamarin Expert Day

---
+++ TravelLanguageAssistant/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TravelLanguageAssistant
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- TravelLanguageAssistant/Services/AzureKeys.cs
+++ TravelLanguageAssistant/Services/AzureKeys.cs
@@ -3,14 +3,15 @@ namespace TravelLanguageAssistant.Services
 {
 	public static class AzureKeys
 	{
-#error You need to set up your API keys.
-		// Start by registering for an account at https://customvision.ai
-		// Then create a new project.
-		// From the settings tab, find:
-		// Prediction Key
-		// Project Id
-		// and update the values below
-		public const string AzureComputerVisonAPIKey = "<API KEY GOES HERE>";
-		public const string AzureSpeechAPIKey = "<API KEY GOES HERE>";
-	}
+
+        // Start by registering for an account at https://customvision.ai
+        // Then create a new project.
+        // From the settings tab, find:
+        // Prediction Key
+        // Project Id
+        // and update the values below
+        public const string AzureComputerVisionEndpoint = "https://westeurope.api.cognitive.microsoft.com/";
+        public const string AzureComputerVisonAPIKey = "76018f68ebb9416fa03a6b502344ceac";
+        public const string AzureSpeechAPIKey = "866d3ba4fdee4de68f25762499c6cd3c";
+    }
 }
